662ec703f3aedd59e6246efb0013830108cd7a2f
misris <misris427@gmail.com>
2018-10-15 19:26:02 +0900

    add single.php

--- single.php
+++ single.php
@@ -1,6 +1,36 @@
 <?php get_header(); ?>
 <div class="main">
   <div class="contents">
+    <?php if(have_posts()): the_post(); ?>
+    <article <?php post_class( 'article' ); ?>>
+      <div class="article-info">
+        <!--投稿日を取得-->
+        <span class="article-date">
+          <i class="fas fa-pencil-alt"></i>
+          <time
+          datetime="<?php echo get_the_date( 'Y-m-d' ); ?>">
+          <?php echo get_the_date(); ?>
+          </time>
+        </span>
+        <!--カテゴリ取得-->
+        <?php if(has_category() ): ?>
+        <span class="article-category">
+          <?php echo get_the_category_list( ' ' ); ?>
+        </span>
+        <?php endif; ?>
+      </div>
+      <!--タイトル-->
+      <h1><?php the_title(); ?></h1>
+      <!--アイキャッチ取得-->
+      <?php if( has_post_thumbnail() ): ?>
+      <div class="article-image">
+        <?php the_post_thumbnail( 'large' ); ?>
+      </div>
+      <?php endif; ?>
+      <!--本文取得-->
+      <?php the_content(); ?>
+    </article>
+    <?php endif; ?>
  
   </div>
   <?php get_sidebar(); ?>
